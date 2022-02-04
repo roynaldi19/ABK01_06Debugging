e6a2cc33ffc4bbddb237151f9e84c372453c7034
roynaldi19 <roynaldi19@gmail.com>
2022-02-04 12:11:25 +0700

    update log

--- app/src/main/java/com/roynaldi19/abk01_06debugging/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/abk01_06debugging/MainActivity.kt
@@ -2,10 +2,26 @@ package com.roynaldi19.abk01_06debugging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logging()
     }
+
+    fun logging() {
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+    }
+
 }
