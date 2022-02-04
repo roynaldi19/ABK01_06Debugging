ffddae68373b93002448730ea004cc9c9e8c24c8
roynaldi19 <roynaldi19@gmail.com>
2022-02-04 11:58:40 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/abk01_06debugging/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.roynaldi19.abk01_06debugging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
